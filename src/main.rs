57a3e0c48264bc60b1b15e17e8f08bc28f2a4a91
denys2 <denys.lempl4@gmail.com>
2026-06-28 16:40:42 +0200

    Add Get Url and index page

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod store;
 
-use axum::routing::post;
-use axum::{Json, Router, extract::State, routing::get};
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    http::StatusCode,
+    response::{Html, Redirect},
+    routing::{get, post},
+};
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
 use std::net::SocketAddr;
@@ -10,6 +15,9 @@ use std::sync::{Arc, Mutex};
 use store::{InMemoryStore, UrlStore};
 
 use serde::{Deserialize, Serialize};
+use tower_http::trace::TraceLayer;
+use tracing::info;
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Clone)]
 struct AppState {
@@ -27,8 +35,20 @@ struct ShortenResponse {
     url_original: String,
 }
 
+fn init_tracing() {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+            "url_shortener=debug,tower_http=debug,axum::rejection=trace".into()
+        }))
+        .init();
+}
+
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+    init_tracing();
+
     let mut store = InMemoryStore::new();
     // Add a sample URL mapping to the store
     store.insert("https://www.rust-lang.org".to_string(), "rust".to_string());
@@ -38,24 +58,27 @@ async fn main() {
     };
 
     let app = Router::new()
-        .route("/", get(root_handler))
+        .route("/", get(index_handler))
+        .route("/health", get(health_check_handler))
         .route("/shorten", post(shorten_handler))
+        .route("/{url_short}", get(redirect_handler))
+        .layer(TraceLayer::new_for_http())
         .with_state(state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Listening on http://{}", addr);
+    info!(%addr, "server listening");
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root_handler(State(state): State<AppState>) -> String {
-    let store = state.store.lock().unwrap();
-    format!(
-        "{} URL(s) stored. Lookup ('rust') = {:?}",
-        store.count(),
-        store.lookup("rust")
-    )
+async fn health_check_handler() -> String {
+    format!("Ok")
+}
+
+async fn index_handler() -> Html<&'static str> {
+    tracing::debug!("serving index page");
+    Html(include_str!("../templates/index.html"))
 }
 
 fn generate_url_short(url_original: &str) -> String {
@@ -74,8 +97,28 @@ async fn shorten_handler(
     let mut store = state.store.lock().unwrap();
     store.insert(url_original.clone(), url_short.clone());
 
+    info!(%url_short, %url_original, "created short url");
+
     Json(ShortenResponse {
         url_short,
         url_original,
     })
+}
+
+async fn redirect_handler(
+    State(state): State<AppState>,
+    Path(url_short): Path<String>,
+) -> Result<Redirect, StatusCode> {
+    let store = state.store.lock().unwrap();
+
+    match store.lookup(&url_short) {
+        Some(url_original) => {
+            info!(%url_short, %url_original, "redirecting to original url");
+            Ok(Redirect::temporary(&url_original))
+        }
+        None => {
+            tracing::warn!(%url_short, "short url not found");
+            Err(StatusCode::NOT_FOUND)
+        }
+    }
 }
