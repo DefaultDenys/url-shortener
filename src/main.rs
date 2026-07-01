7d3d28028446898f75d17f078f032add55fccc09
denys2 <denys.lempl4@gmail.com>
2026-07-01 11:56:42 +0200

    Change server address

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ async fn main() {
 
     let app = router::build_router(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     info!(%addr, "server listening");
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
